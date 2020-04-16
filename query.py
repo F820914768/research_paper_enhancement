b1aa543d5d2f394dd3314a849c9f89e33c4cd165
F820914768 <820914768@qq.com>
2020-04-17 02:47:16 +0800

    use to query table

--- query.py
+++ query.py
@@ -1,5 +1,7 @@
-import sqlite3
+# -*- coding: utf-8 -*-
 
+import sqlite3
+import pandas as pd
 
 if __name__ == '__main__':
     conn = sqlite3.connect('project.db')
@@ -7,12 +9,18 @@ if __name__ == '__main__':
 
 
     query = """
-            SELECT i.ID, q.Institution_Name, q.Score
-            FROM institution as i, QS as q
-            WHERE i.Institution_Name = q.Institution_Name
+            SELECT i.ID, q.Institution_Name, q.Score, i.Institution_Name
+            FROM institution as i LEFT JOIN QS as q ON q.Institution_Name = i.Institution_Name
 
     """
 
     cursor.execute(query)
     data = cursor.fetchall()
-    print(len(data))
+    
+
+    df = pd.DataFrame(data)
+    df.to_csv('institution_ranking.csv')
+    print(df[2].isnull().sum())
+
+
+        
